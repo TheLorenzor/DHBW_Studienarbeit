396cc053b04229ef2ba887e99297b9382a84505b
the_loraxxz <the_loraxxz@web.de>
2023-01-29 17:17:21 +0100

    starting with traffic signs

--- detect_files.py
+++ detect_files.py
@@ -1,7 +1,7 @@
 import torch
 import torchvision
 model = torch.hub.load('ultralytics/yolov5', 'custom',
-                       path='D:\Programmieren\\Uni\Studienarbeit\yolov5\\runs\\train\exp4\weights\\best.pt')
+                       path='D:\Programmieren\\Uni\Studienarbeit\yolov5\\runs\\train\exp7\weights\\best.pt')
 
 im = "D:\Programmieren\\Uni\Studienarbeit\\test.jpg"
 
@@ -9,4 +9,4 @@ results = model(im)
 print(results.pandas().xyxy[0])
 
 results.print()
-results.save()  # or .show()
+results.show()  # or .show()
