90a0a4429b1ae482058f2a20b8a1bef9be703e76
the_loraxxz <the_loraxxz@web.de>
2023-02-09 21:33:10 +0100

    started with templating workin on index site

--- traffic-light/main.py
+++ traffic-light/main.py
@@ -1,16 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 from frontend import routes
-
 load_dotenv()
-app = Flask(__name__)
+
+template_folder_path = os.path.join('..', 'data', 'frontend_websites','templates')
+static_path = os.path.join(template_folder_path,'..','static_files')
+app = Flask(__name__, template_folder=template_folder_path,static_folder=static_path)
+
 
 @app.route('/')
 def root():
-    return '<p>TEst</p>'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     print(os.getenv('DEBUG'))
